b299025ea71cb9561f451190cca69cbd2429dca5
Navyakamal <158019137+Navyakamal@users.noreply.github.com>
2024-04-11 21:00:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [num,setNum] = useState(0)
+  function incre(){
+    setNum(num+1)
+  }
+  function decre(){
+    setNum(num-1)
+  }
+  function reset(){
+    setNum(0)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='container w-75 shadow'>
+        <h1 className='text-center'>Counter App</h1>
+        <h2 className='text-center'>{num}</h2>
+        <button type="button" class="btn btn-primary" onClick={incre}>+1</button>
+        <button type="button" class="btn btn-secondary" onClick={reset}>Reset</button>
+        <button type="button" class="btn btn-primary" onClick={decre}>-1</button>
+
+      </div>   
+ </div>
   );
 }
 
